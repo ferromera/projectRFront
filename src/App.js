0bdff12ca0272916b5b6518ba8d43212a9b79e35
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2022-06-12 19:25:35 -0300

    move constants

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import WatchedMovies from "./pages/WatchedMovies";
 import AllMovies from "./pages/AllMovies";
 import NewMovies from "./pages/NewMovies";
 
+//Prod
+export const BACKEND_HOST = 'https://projectr-backend.herokuapp.com'
+export const USER_ID = 4;
+//Dev
+//export const BACKEND_HOST = 'http://localhost:8080'
+//export const USER_ID = 3;
 
 function App() {
     return (
--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {WatchedContextProvider} from "./store/WatchedContext";
 
-//Prod
-export const BACKEND_HOST = 'https://projectr-backend.herokuapp.com'
-export const USER_ID = 4;
-//Dev
-//export const BACKEND_HOST = 'http://localhost:8080'
-//export const USER_ID = 3;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
