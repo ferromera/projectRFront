69e35ef2195f82bd8bc47880da86517150566d29
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2022-07-15 18:49:25 -0300

    add missing files

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,54 @@
+import {
+    IconButton,
+    InputAdornment,
+    InputBase,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+import classes from './SearchBar.module.css'
+import { useNavigate } from "react-router-dom";
+
+function SearchBar(props) {
+    const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        navigate("/movies?search=" + searchQuery);
+    };
+
+    function handleChange(event) {
+        setSearchQuery(event.target.value);
+    }
+
+    return (
+        <div style={{display:"inline-block"}}>
+            <form onSubmit={handleSubmit}>
+                <InputBase
+                    placeholder="Search…"
+                    inputProps={{ "aria-label": "search" }}
+                    id="search-bar"
+                    label=""
+                    size="small"
+                    onChange={handleChange}
+                    value={searchQuery}
+                    color="secondary"
+                    className={classes.search}
+                    classes={{ focused: classes.searchFocused }}
+                    startAdornment={(
+                        <InputAdornment position="start">
+                            <SearchIcon sx={{ color: "white" }} />
+                        </InputAdornment>
+                    )}
+            
+                />
+                
+
+                <IconButton type="submit" aria-label="search">
+                    
+                </IconButton>
+            </form>
+        </div>
+    );
+}
+
+export default SearchBar;
